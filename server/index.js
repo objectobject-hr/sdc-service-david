c8b7f7d120fc9f802a02baceeb7566030b4c7f3e
David Kim <hyomithetech@gmail.com>
2019-12-19 11:25:14 -0800

    Implement changes on the legacy code

--- server/index.js
+++ server/index.js
@@ -1,46 +1,45 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const Reviews = require('../db/data').Reviews;
-const Zips = require('../db/data').Zips;
-const path = require('path');
-
+const express = require("express");
+const proxy = require("http-proxy-middleware");
+const bodyParser = require("body-parser");
+const Reviews = require("../db/data").Reviews;
+const Zips = require("../db/data").Zips;
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3004;
 
-app.use(cors());
+app.use(proxy);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get('/reviews/:id', async (req, res) => {
+app.get("/reviews/:id", async (req, res) => {
   try {
-    const data = await Reviews.findAll({ where: { ListingId: req.params.id } })
+    const data = await Reviews.findAll({ where: { ListingId: req.params.id } });
 
-    if(!data){
-      res.status(404).send()
+    if (!data) {
+      res.status(404).send();
     }
 
-    res.status(200).send(data)
+    res.status(200).send(data);
   } catch (e) {
-    res.status(500).res.send(e)
+    res.status(500).res.send(e);
   }
 });
 
-app.get('/zips/:id', async (req, res) => {
+app.get("/zips/:id", async (req, res) => {
   try {
-    const data = await Zips.findAll({ where: { ListingId: req.params.id } })
+    const data = await Zips.findAll({ where: { ListingId: req.params.id } });
 
-    if(!data){
-      res.status(404).send()
+    if (!data) {
+      res.status(404).send();
     }
 
-    res.status(200).send(data)
+    res.status(200).send(data);
   } catch (e) {
-    res.status(500).res.send(e)
+    res.status(500).res.send(e);
   }
-})
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port -> ${port} <-`);
